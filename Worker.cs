bc83cb6303967c2945cfa4eede0fe81edceea136
Rick_C137 <jayanta.chakravorty@ltts.com>
2020-06-07 18:29:35 +0530

    trying ip

--- Worker.cs
+++ Worker.cs
@@ -21,7 +21,7 @@ namespace GrpcClient
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var channel = GrpcChannel.ForAddress("https://service2-grpcserver:443");
+            var channel = GrpcChannel.ForAddress("https://172.30.14.99:443");
             var client = new Greeter.GreeterClient(channel);
             try
             {
