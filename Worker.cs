81375a25a8c7706819f55f557309f4e685206a0e
Rick_C137 <jayanta.chakravorty@ltts.com>
2020-06-07 20:21:31 +0530

    some thing

--- Worker.cs
+++ Worker.cs
@@ -38,7 +38,7 @@ namespace GrpcClient
                     {
                         await Task.Delay(5000, stoppingToken);
                         _logger.LogInformation($"Calling test webapi ");
-                        var response = httpClient.GetAsync("http://hellomachine:80/test").Result;
+                        var response = httpClient.GetAsync("http://hellomachine:8080/test").Result;
                         _logger.LogInformation(await response.Content.ReadAsStringAsync());
 
                         await Task.Delay(5000, stoppingToken);
